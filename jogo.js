eb96cac6db0f4edd2570277cebaa07dbe53e9ee1
João Inácio <joaoinacio206@protonmail.com>
2021-02-03 09:34:31 -0300

    Criação randômica

--- jogo.js
+++ jogo.js
@@ -3,10 +3,27 @@ var largura = 0
 function ajustaTamanhoJogo() {
     altura = window.innerHeight
     largura = window.innerWidth
-    console.log(altura, largura)
+    console.log(largura, altura)
 }
 ajustaTamanhoJogo()
-console.log(altura, largura)
+// console.log(altura, largura)
 
+function posicaoRandomica() {
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+    posicaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
 
+    console.log(posicaoX, posicaoY)
+
+    // Criar HTML
+    var mosquito = document.createElement('img')
+    mosquito.src = 'img/mosca.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+
+    document.body.appendChild(mosquito)
+}
 
