a72320452513b7c321df889275f664a736e830b1
João Inácio <joaoinacio206@protonmail.com>
2021-02-04 09:38:38 -0300

    Tempo de aparição

--- jogo.js
+++ jogo.js
@@ -9,6 +9,10 @@ ajustaTamanhoJogo()
 // console.log(altura, largura)
 
 function posicaoRandomica() {
+    // romover os mosquito
+    if (document.getElementById('mosquito')) {
+        document.getElementById('mosquito').remove()
+    }
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
     posicaoX = posicaoX < 0 ? 0 : posicaoX
@@ -23,7 +27,7 @@ function posicaoRandomica() {
     mosquito.style.left = posicaoX + 'px'
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
-
+    mosquito.id = 'mosquito'
     document.body.appendChild(mosquito)
 }
 function tamanhoAleatorio() {
