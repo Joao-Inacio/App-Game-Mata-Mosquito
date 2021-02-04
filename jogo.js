c77f68eeac878bdb0d764fbd4ff6b2e67014f4d2
João Inácio <joaoinacio206@protonmail.com>
2021-02-04 18:08:13 -0300

    implementação de vidas

--- jogo.js
+++ jogo.js
@@ -1,5 +1,6 @@
 var altura = 0
 var largura = 0
+var vidas = 1
 function ajustaTamanhoJogo() {
     altura = window.innerHeight
     largura = window.innerWidth
@@ -12,6 +13,13 @@ function posicaoRandomica() {
     // romover os mosquito
     if (document.getElementById('mosquito')) {
         document.getElementById('mosquito').remove()
+        if(vidas > 3 ){
+            alert('Interromper o jogo (game over)')
+        }
+        else{ 
+        document.getElementById('v' + vidas).src = "img/coracao_vazio.png"
+        vidas++
+        }
     }
     var posicaoX = Math.floor(Math.random() * largura) - 90
     var posicaoY = Math.floor(Math.random() * altura) - 90
@@ -28,6 +36,10 @@ function posicaoRandomica() {
     mosquito.style.top = posicaoY + 'px'
     mosquito.style.position = 'absolute'
     mosquito.id = 'mosquito'
+    mosquito.onclick = function () {
+        this.remove()
+    }
+
     document.body.appendChild(mosquito)
 }
 function tamanhoAleatorio() {
