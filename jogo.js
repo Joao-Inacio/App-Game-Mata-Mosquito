a9b58ab468c5b5ff82f6b85392c5433a4928542b
João Inácio <joaoinacio206@protonmail.com>
2021-02-05 17:44:14 -0300

    Pagina de vitória

--- jogo.js
+++ jogo.js
@@ -13,7 +13,7 @@ var cronometro = setInterval(function(){
     if (tempo < 0) {
         clearInterval(cronometro)
         clearInterval(criarMosquito)
-        alert("Vitoria")
+        window.location.href = 'vitoria.html'
     } 
     else{ 
     document.getElementById('cronometro').innerHTML = tempo
